d5b05138ef224f86dd557d22a21d0da4c235536b
Agustinpena2 <112119786+Agustinpena2@users.noreply.github.com>
2022-09-21 22:18:24 -0600

    Create Propotipo.py

---
+++ Propotipo.py
@@ -0,0 +1,6 @@
+#TEA
+print("Este programa es para estimar la cantidad de metros cubicos de precipitacion de una area determinada")
+area = input("El area de interes (metros cuadrados) es: ")
+precipitacion = input("La precipitacion actual (mm) es de: ")
+Total = float(area)*(float(precipitacion)*0.001)
+print (Total)
