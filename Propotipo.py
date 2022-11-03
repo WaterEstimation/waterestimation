6d4b30788c8251ac27793491d31fa7027dc3e9f3
Agustinpena2 <112119786+Agustinpena2@users.noreply.github.com>
2022-11-03 14:40:41 -0600

    Update Propotipo.py

--- Propotipo.py
+++ Propotipo.py
@@ -4,3 +4,25 @@ area = input("El area de interes (metros cuadrados) es: ")
 precipitacion = input("La precipitacion actual (mm) es de: ")
 Total = float(area)*(float(precipitacion)*0.001)
 print (Total)
+
+import datetime
+fechas = ['2022-11-03', '2022-11-03', '2022-11-03', '2022-11-03']
+fecha = datetime.date.today()
+year = fecha.year
+for y in precipitacion:
+    if (y.anno == 2016): jhajsahdjk
+    elif (y.anno == 2017): hghdagshd
+    elif (y.anno == 2018):
+    elif (y.anno == 2019)
+    elif (y.anno == 2020)
+    elif (y.anno == 2021)
+    elif (y.anno == 2022)
+    .
+    else: sdhshdkjshf
+
+for x in range(2016, year):
+    print(x)
+
+    #print(fechas)
+#print(fecha)
+#print(year)
